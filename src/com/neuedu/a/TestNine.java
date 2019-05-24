ffa9fe52c7887ce8e91cf0282537961b232ad37e
cqwiuc <3200944489@qq.com>
2019-05-24 19:35:29 +0800

    z

--- src/com/neuedu/a/TestNine.java
+++ src/com/neuedu/a/TestNine.java
@@ -1,2 +1,22 @@
+package com.neuedu.a;
 
+import java.util.Scanner;
+
+public class TestNine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        boolean flag = false;
+        int temp = 0;
+        while (input%10>0){
+            temp += input%10;
+            input = input/10;
+        }
+        temp =temp + input;
+        if(temp%9==0){
+            flag = true;
+        }
+        System.out.println(flag+"能整除:不能整除");
+    }
+}
 
