7690288ed9bda8175b7f877a6c1794077b54b29a
cqwiuc <3200944489@qq.com>
2019-05-24 19:14:53 +0800

    z

---
+++ src/com/neuedu/a/TestOne1.java
@@ -0,0 +1,15 @@
+package com.neuedu.a;
+
+public class TestOne1 {
+    public static void main(String[] args) {
+        for(int i=100;i<=999;i++){
+            int a=i/100;
+            int b=i%100/10;
+            int c=i%10;
+            if(a*a*a+b*b*b+c*c*c==i){
+                System.out.println("水仙花数--->"+i);
+            }
+        }
+    }
+}
+
