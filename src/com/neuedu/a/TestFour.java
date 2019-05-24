9933c327da94948c19eb8a3d836e3d9a954b0990
cqwiuc <3200944489@qq.com>
2019-05-24 19:24:37 +0800

    z

---
+++ src/com/neuedu/a/TestFour.java
@@ -0,0 +1,18 @@
+package com.neuedu.a;
+
+public class TestFour {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=100;i++)
+        {
+            if(i%2==0){
+                System.out.println(i+"是偶数");
+            }else{
+                System.out.println(i+"是奇数");
+            }
+        }
+
+    }
+
+}
+
